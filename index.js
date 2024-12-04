e971e8734ebd660f2ab390983d3996c04ed91ae6
Alex <alexwisco29@gmail.com>
2024-12-04 11:43:48 -0500

    done 3.4

--- index.js
+++ index.js
@@ -18,19 +18,24 @@ let persons = [
         id: "3",
         name: "Spongebob2",
         number: "345"
+      },
+      {
+        id: "4",
+        name: "Spong",
+        number: "456"
       }
 ]
 
 // routes
 // Main page 
 app.get('/', (request, response) => {
-    response.send('<h1> .Hello World. <h1>')
-})
+    response.send('<h1> Hello World <h1>')
+}) // homepage
 
 // retrieve all items in phonebook
 app.get('/api/persons', (request, response) => {
     response.json(persons)
-  })
+  }) // all
 
 // retrieve number of people in phonebook
 app.get('/info', (request, response) => {
@@ -41,7 +46,7 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p>
       <p>${d}</p>`)
   
-})
+}) // length of phonebook
 
 // retrieving individual data - colon defines parameters on the path
 app.get('/api/persons/:id', (request, response) => {
@@ -53,9 +58,17 @@ app.get('/api/persons/:id', (request, response) => {
   if (person) {
     response.json(person)
   } else {
-    response.status(404).end()
+    response.status(404).end() // end() says request should be answered without data
   }
-  
+}) // individual
+
+// deleting individual contact
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  console.log('requesting to delete id: ', id)
+  persons = persons.filter(person => person.id !== id) // return new with everyone but id match
+  response.status(204).end()
+
 })
 
 const PORT = 3001
