773e1b827f677fc249a628b973ca3c4d0411a898
Alex <alexwisco29@gmail.com>
2024-12-04 11:31:12 -0500

    done 3.3

--- index.js
+++ index.js
@@ -12,24 +12,51 @@ let persons = [
       {
         id: "2",
         name: "Angela",
-        important: "234"
+        number: "234"
       },
       {
         id: "3",
-        name: "Spongebob",
+        name: "Spongebob2",
         number: "345"
       }
 ]
 
 // routes
-
+// Main page 
 app.get('/', (request, response) => {
     response.send('<h1> .Hello World. <h1>')
 })
 
+// retrieve all items in phonebook
 app.get('/api/persons', (request, response) => {
     response.json(persons)
   })
+
+// retrieve number of people in phonebook
+app.get('/info', (request, response) => {
+  // Timestamp code:
+  const d = Date()
+  console.log("Date: ", d)
+    //console.log("timestamp: ", request.date)
+    response.send(`<p>Phonebook has info for ${persons.length} people</p>
+      <p>${d}</p>`)
+  
+})
+
+// retrieving individual data - colon defines parameters on the path
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id // id value of the path param accessed with request object
+  // that tells the request information
+  const person = persons.find(person => person.id === id)
+
+  // error checks before returning
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+  
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
